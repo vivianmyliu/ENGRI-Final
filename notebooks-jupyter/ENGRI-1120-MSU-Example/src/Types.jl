961576528cccf0d4e55a0ffe234fe88ca5bb90ed
Vivian Liu <113494966+vivianmyliu@users.noreply.github.com>
2022-12-04 14:50:11 -0500

    Deleted unnecessary files
    (deleted everything irrelevant to project)

--- notebooks-jupyter/ENGRI-1120-MSU-Example/src/Types.jl
+++
@@ -1,9 +0,0 @@
-mutable struct MSULatticeModel
-
-    # data -
-    connectivity::Array{Int64,2}
-    data::Array{Float64,1}
-
-    # constructor -
-    MSULatticeModel() = new();
-end
--- notebooks-jupyter/ENGRI-1120-Toy-FBA-Example/ENGRI-1120-Example-CodeLib.jl
+++
@@ -1,4 +0,0 @@
-# my codes -
-include(joinpath("src","Stoichiometric.jl"))
-include(joinpath("src", "Parser.jl"))
-include(joinpath("src","Flux.jl"))
